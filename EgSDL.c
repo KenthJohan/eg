49db365c89c70955194846c2cd9763bd9959850a
Johan Söderlind Åström <johan.soderlind.astrom@gmail.com>
2022-06-12 01:08:18 +0200

    refactor

--- EgSDL.c
+++ EgSDL.c
@@ -8,9 +8,6 @@
 #include <SDL2/SDL.h>
 
 
-
-
-
 void eg_gl_make_current(ecs_world_t * world, ecs_entity_t e)
 {
 	Eg_SDL_Window const * win = ecs_get(world, e, Eg_SDL_Window);
@@ -41,72 +38,6 @@ void eg_gl_swap_buffer(ecs_world_t * world, ecs_entity_t e)
 
 
 
-
-
-
-
-static void System_Update_Title(ecs_iter_t *it)
-{
-	EG_ITER_INFO(it);
-	Eg_SDL_Window *w = ecs_term(it, Eg_SDL_Window, 1);
-	EgTitle *title = ecs_term(it, EgTitle, 2);
-	for (int i = 0; i < it->count; i ++)
-	{
-		if(w[i].window)
-		{
-			SDL_SetWindowTitle(w[i].window, title[i].value);
-		}
-	}
-}
-
-
-
-
-
-
-
-
-
-
-static void System_Change_Window_Size(ecs_iter_t *it)
-{
-	EG_ITER_INFO(it);
-	Eg_SDL_Window *w = ecs_term(it, Eg_SDL_Window, 1);
-	EgRectangleI32 *r = ecs_term(it, EgRectangleI32, 2);
-	for (int i = 0; i < it->count; i ++)
-	{
-		//https://wiki.libsdl.org/SDL_SetWindowSize
-		if (w[i].window)
-		{
-			SDL_SetWindowSize(w[i].window, r[i].width, r[i].height);
-		}
-	}
-}
-
-
-static void System_Update_Window_Size(ecs_iter_t *it)
-{
-	//EG_ITER_INFO(it);
-	Eg_SDL_Window *w = ecs_term(it, Eg_SDL_Window, 1);
-	EgRectangleI32 *r = ecs_term(it, EgRectangleI32, 2);
-	for (int i = 0; i < it->count; i ++)
-	{
-		//https://wiki.libsdl.org/SDL_SetWindowSize
-		if (w[i].window)
-		{
-			int width;
-			int height;
-			SDL_GetWindowSize(w[i].window, &width, &height);
-			r[i].width = width;
-			r[i].height = height;
-		}
-	}
-}
-
-
-
-
-
 void EgSDLImport(ecs_world_t *world)
 {
 	ECS_MODULE(world, EgSDL);
@@ -116,73 +47,6 @@ void EgSDLImport(ecs_world_t *world)
 	ECS_IMPORT(world, EgEvents);
 	ECS_IMPORT(world, EgQuantities);
 	ecs_set_name_prefix(world, "Eg");
-
-
 	SDL_Init(SDL_INIT_VIDEO);
-
-
-
-
-
-	//ECS_TRIGGER(world, System_Destroy_Window, EcsOnRemove, Eg_SDL_Window);
-
-
-
-
-	ecs_observer_init(world, &(ecs_observer_desc_t) {
-	.filter.terms = {
-	{ .id = ecs_id(Eg_SDL_Window), .inout = EcsOut },
-	{ .id = ecs_id(EgRectangleI32), .inout = EcsIn}
-	},
-	.events = {EcsOnSet},
-	.callback = System_Change_Window_Size
-	});
-
-	ecs_observer_init(world, &(ecs_observer_desc_t) {
-	.filter.terms = {
-	{ .id = ecs_id(Eg_SDL_Window), .inout = EcsOut },
-	{ .id = ecs_id(EgTitle), .inout = EcsIn}
-	},
-	.events = {EcsOnSet},
-	.callback = System_Update_Title
-	});
-
-
-
-	ecs_system_init(world, &(ecs_system_desc_t) {
-	.query.filter.terms = {
-	{ .id = ecs_id(Eg_SDL_Window), .inout = EcsIn },
-	{ .id = ecs_id(EgRectangleI32), .inout = EcsOut}
-	},
-	.entity.add = {EcsOnUpdate},
-	.callback = System_Update_Window_Size
-	});
-
-
-
-
-	/*
-	ECS_OBSERVER(world, System_Create_Window, EcsOnAdd,
-	[inout] EgWindow,
-	[in]    EgRectangleI32);
-	ECS_OBSERVER(world, System_Change_Window_Size, EcsOnSet,
-	[out] Eg_SDL_Window,
-	[in]  EgRectangleI32);
-	ECS_OBSERVER(world, System_Update_Title, EcsOnSet,
-	[out] Eg_SDL_Window,
-	[in]  EgTitle);
-	ECS_SYSTEM(world, System_Update_Window, EcsOnUpdate,
-	[inout] Eg_SDL_Window,
-	[inout] EgWindow);
-	ECS_SYSTEM(world, System_Update_Window_Size, EcsOnUpdate,
-	[in]  Eg_SDL_Window,
-	[out] EgRectangleI32);
-	ECS_SYSTEM(world, System_Update_UserEvent, EcsOnUpdate,
-	[out] EgUserEvent($));
-	*/
-
-
-
-	
 }
 
--- EgSDLW.c
+++ EgSDLW.c
@@ -72,7 +72,6 @@ static void System_Destroy_Window(ecs_iter_t *it)
 }
 
 
-
 static void System_Update_Window(ecs_iter_t *it)
 {
 	Eg_SDL_Window *s = ecs_term(it, Eg_SDL_Window, 1);
@@ -160,6 +159,57 @@ static void System_Update_UserEvent(ecs_iter_t *it)
 }
 
 
+static void System_Update_Title(ecs_iter_t *it)
+{
+	EG_ITER_INFO(it);
+	Eg_SDL_Window *w = ecs_term(it, Eg_SDL_Window, 1);
+	EgTitle *title = ecs_term(it, EgTitle, 2);
+	for (int i = 0; i < it->count; i ++)
+	{
+		if(w[i].window)
+		{
+			SDL_SetWindowTitle(w[i].window, title[i].value);
+		}
+	}
+}
+
+
+static void System_Change_Window_Size(ecs_iter_t *it)
+{
+	EG_ITER_INFO(it);
+	Eg_SDL_Window *w = ecs_term(it, Eg_SDL_Window, 1);
+	EgRectangleI32 *r = ecs_term(it, EgRectangleI32, 2);
+	for (int i = 0; i < it->count; i ++)
+	{
+		//https://wiki.libsdl.org/SDL_SetWindowSize
+		if (w[i].window)
+		{
+			SDL_SetWindowSize(w[i].window, r[i].width, r[i].height);
+		}
+	}
+}
+
+
+static void System_Update_Window_Size(ecs_iter_t *it)
+{
+	//EG_ITER_INFO(it);
+	Eg_SDL_Window *w = ecs_term(it, Eg_SDL_Window, 1);
+	EgRectangleI32 *r = ecs_term(it, EgRectangleI32, 2);
+	for (int i = 0; i < it->count; i ++)
+	{
+		//https://wiki.libsdl.org/SDL_SetWindowSize
+		if (w[i].window)
+		{
+			int width;
+			int height;
+			SDL_GetWindowSize(w[i].window, &width, &height);
+			r[i].width = width;
+			r[i].height = height;
+		}
+	}
+}
+
+
 
 void EgSDLWImport(ecs_world_t *world)
 {
@@ -202,6 +252,38 @@ void EgSDLWImport(ecs_world_t *world)
 	.entity.add = {EcsOnUpdate},
 	.callback = System_Update_UserEvent
 	});
+
+
+
+	ecs_observer_init(world, &(ecs_observer_desc_t) {
+	.filter.terms = {
+	{ .id = ecs_id(Eg_SDL_Window), .inout = EcsOut },
+	{ .id = ecs_id(EgRectangleI32), .inout = EcsIn}
+	},
+	.events = {EcsOnSet},
+	.callback = System_Change_Window_Size
+	});
+
+	ecs_observer_init(world, &(ecs_observer_desc_t) {
+	.filter.terms = {
+	{ .id = ecs_id(Eg_SDL_Window), .inout = EcsOut },
+	{ .id = ecs_id(EgTitle), .inout = EcsIn}
+	},
+	.events = {EcsOnSet},
+	.callback = System_Update_Title
+	});
+
+
+
+	ecs_system_init(world, &(ecs_system_desc_t) {
+	.query.filter.terms = {
+	{ .id = ecs_id(Eg_SDL_Window), .inout = EcsIn },
+	{ .id = ecs_id(EgRectangleI32), .inout = EcsOut}
+	},
+	.entity.add = {EcsOnUpdate},
+	.callback = System_Update_Window_Size
+	});
+
 
 
 	ecs_singleton_set(world, EgUserEvent, { 0 });
