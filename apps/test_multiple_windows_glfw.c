3db90f6c48884c0a30330d60cbd394f603a5c174
Johan Söderlind Åström <johan.soderlind.astrom@gmail.com>
2022-07-15 20:31:28 +0200

    update

--- apps/test_multiple_windows_glfw.c
+++ apps/test_multiple_windows_glfw.c
@@ -1,5 +1,4 @@
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_opengl.h>
+#include <GLFW/glfw3.h>
 
 #define HANDMADE_MATH_IMPLEMENTATION
 #define HANDMADE_MATH_NO_SSE
@@ -8,7 +7,6 @@
 #include "../sokol/sokol_gfx.h"
 #include "../sokol/sokol_glue.h"
 #include "../sokol/util/sokol_debugtext.h"
-#include "debugtext-context-sapp.glsl.h"
 
 /* different clear colors for 3 windows */
 sg_pass_action pass_actions[3] = {
@@ -28,53 +26,24 @@ typedef struct {
 	hmm_mat4 mvp;
 } params_t;
 
-#define WINDOW_COUNT 2
-
-
-
-int any_notnull(void ** objects, int n)
-{
-	for (int i = 0; i < n; i++)
-	{
-		if (objects[i] != NULL){return 1;}
-	}
-	return 0;
-}
-
-
-int main(int argc, char **argv)
-{
+int main() {
 	/* setup GLFW and create a couple of windows each with its own GL context behind */
 	const int WIDTH = 512;
 	const int HEIGHT = 384;
-
-	(void)argc, (void)argv;
-	if(SDL_Init(SDL_INIT_VIDEO) < 0)
-	{
-		fprintf(stderr, "SDL_Init error: %s\n", SDL_GetError());
-		return 1;
-	}
-
-
-	SDL_Window *windows[WINDOW_COUNT];
-	SDL_GLContext contexts[WINDOW_COUNT];
-	for(int i = 0; i < WINDOW_COUNT; ++i)
-	{
-		char title[32];
-		snprintf(title, sizeof(title), "window%d", i);
-		windows[i] = SDL_CreateWindow(title, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_OPENGL);
-		if(!windows[i])
-		{
-			fprintf(stderr, "SDL_CreateWindow error: %s\n", SDL_GetError());
-			return 1;
-		}
-		contexts[i] = SDL_GL_CreateContext(windows[i]);
-		if(!contexts[i])
-		{
-			fprintf(stderr, "SDL_GL_CreateContext error: %s\n", SDL_GetError());
-			return 1;
-		}
-	}
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	GLFWwindow* w[3];
+	w[0] = glfwCreateWindow(WIDTH, HEIGHT, "Window 0", 0, 0);
+	glfwSetWindowPos(w[0], 40, 40);
+	w[1] = glfwCreateWindow(WIDTH, HEIGHT, "Window 1", 0, 0);
+	glfwSetWindowPos(w[1], 40+WIDTH, 40);
+	w[2] = glfwCreateWindow(WIDTH, HEIGHT, "Window 2", 0, 0);
+	glfwSetWindowPos(w[2], 40+WIDTH/2, 40+HEIGHT);
+	glfwMakeContextCurrent(w[0]);
+	glfwSwapInterval(1);
 
 	/* setup sokol-gfx */
 	sg_setup(&(sg_desc) {0});
@@ -124,10 +93,9 @@ int main(int argc, char **argv)
 	sg_context ctx[3];
 	sg_bindings bind[3] = { {0} };
 	sg_pipeline pip[3] = { {0} };
-	for (int i = 0; i < 3; i++)
-	{
+	for (int i = 0; i < 3; i++) {
 		/* for each GL context, setup a sokol context */
-		SDL_GL_MakeCurrent(windows[1], contexts[1]);
+		glfwMakeContextCurrent(w[i]);
 		ctx[i] = sg_setup_context();
 		/* create the usual resources per context (buffers, shader, pipeline) */
 		bind[i].vertex_buffers[0] = sg_make_buffer(&(sg_buffer_desc){
@@ -189,29 +157,7 @@ int main(int argc, char **argv)
 	/* run until all windows are closed */
 	params_t vs_params;
 	float rx = 0.0f, ry = 0.0f;
-	while (any_notnull((void**)windows, WINDOW_COUNT))
-	{
-
-
-		SDL_Event ev;
-		while(SDL_PollEvent(&ev))
-		{
-			if(ev.type == SDL_KEYDOWN && ev.key.keysym.sym == SDLK_q)
-			{
-				break;
-			}
-		}
-
-
-		/*
-		if (ev.type == SDL_WINDOWEVENT && ev.window.event == SDL_WINDOWEVENT_CLOSE)
-		{
-			if (SDL_GetWindowID(myWindowA) == ev.window.windowID)
-			{
-			}
-		}
-		*/
-
+	while (w[0] || w[1] || w[2]) {
 		/* rotated model matrix */
 		rx += 1.0f; ry += 2.0f;
 		hmm_mat4 rxm = HMM_Rotate(rx, HMM_Vec3(1.0f, 0.0f, 0.0f));
@@ -219,19 +165,16 @@ int main(int argc, char **argv)
 		hmm_mat4 model = HMM_MultiplyMat4(rxm, rym);
 		vs_params.mvp = HMM_MultiplyMat4(view_proj, model);
 
-		for (int i = 0; i < 3; i++)
-		{
-			if (windows[i])
-			{
+		for (int i = 0; i < 3; i++) {
+			if (w[i]) {
 				/* switch GL context and sokol context */
-				SDL_GL_MakeCurrent(windows[i], contexts[i]);
+				glfwMakeContextCurrent(w[i]);
 				sg_activate_context(ctx[i]);
 
 				/* do the usual pass rendering */
-				int w;
-				int h;
-				SDL_GetWindowSize(windows[i], &w, &h);
-				sg_begin_default_pass(&pass_actions[i], w, h);
+				int cur_width, cur_height;
+				glfwGetFramebufferSize(w[i], &cur_width, &cur_height);
+				sg_begin_default_pass(&pass_actions[i], cur_width, cur_height);
 				sg_apply_pipeline(pip[i]);
 				sg_apply_bindings(&bind[i]);
 				sg_apply_uniforms(SG_SHADERSTAGE_VS, 0, &SG_RANGE(vs_params));
@@ -239,19 +182,19 @@ int main(int argc, char **argv)
 				sg_end_pass();
 				sg_commit();
 
-				SDL_GL_SwapWindow(windows[1]);
-				/*
-				if (glfwWindowShouldClose(w[i]))
-				{
+				glfwSwapBuffers(w[i]);
+				if (glfwWindowShouldClose(w[i])) {
+					/* this will destroy all resources created in this context */
 					sg_discard_context(ctx[i]);
-					SDL_GL_DeleteContext(contexts[i]);
-					SDL_DestroyWindow(windows[i]);
+					glfwDestroyWindow(w[i]);
 					w[i] = 0;
 				}
-				*/
 			}
 		}
+		glfwPollEvents();
 	}
 	sg_shutdown();
+	glfwTerminate();
 	return 0;
 }
+
---
+++ apps/vulkantest/30_multisampling.pro
@@ -0,0 +1,19 @@
+TEMPLATE = app
+CONFIG += console
+CONFIG -= app_bundle
+CONFIG -= qt
+
+DEFINES = ""
+DEFINES += __USE_MINGW_ANSI_STDIO=1
+
+QMAKE_CFLAGS = ""
+QMAKE_CFLAGS += -Wno-unused-function
+QMAKE_CFLAGS += -Wno-unused-parameter
+QMAKE_CFLAGS += -Wno-missing-braces
+QMAKE_CFLAGS += -Wno-missing-field-initializers
+
+SOURCES += 30_multisampling.cpp
+
+
+LIBS += -LC:/VulkanSDK/1.3.216.0/Lib
+LIBS += -lglfw3 -lgdi32 -lvulkan-1
