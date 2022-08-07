8be71e63f05176d41d6200b14a232c748be52a06
Johan Söderlind Åström <johan.soderlind.astrom@gmail.com>
2022-08-07 14:47:30 +0200

    update

--- apps/vulkantest/render1.h
+++ apps/vulkantest/render1.h
@@ -10,6 +10,10 @@ ecs_entity_t render1_init(ecs_world_t * world);
 VkSampleCountFlagBits render1_get_max_usable_sample_count(VkPhysicalDevice physicalDevice);
 VkPhysicalDevice render1_pick_physical_device(ecs_world_t * world);
 
+
+VkCommandBuffer beginSingleTimeCommands(VkDevice device, VkCommandPool commandPool);
+void endSingleTimeCommands(VkDevice device, VkCommandBuffer commandBuffer, VkQueue graphicsQueue, VkCommandPool commandPool);
+
 #ifdef __cplusplus
 }
 #endif
