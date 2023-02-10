1d5a2d859cf302985a59a6f0c3e29b3f4019b697
Johan Söderlind Åström <johan.soderlind.astrom@gmail.com>
2023-02-10 22:52:19 +0100

    update precedence

--- tools/setup/asts.c
+++ tools/setup/asts.c
@@ -40,10 +40,8 @@ EXP      | TERMINATOR  | ?          | POPX
 typedef enum
 {
 PARSE_STATE_UNKNOWN,
-PARSE_STATE_EXPL,
 PARSE_STATE_EXPR,
 PARSE_STATE_EXPO,
-PARSE_STATE_POP,
 PARSE_STATE_COUNT
 } parse_state_t;
 
@@ -65,10 +63,8 @@ char const * parse_state_t_tostr(parse_state_t state)
 	switch (state)
 	{
 	case PARSE_STATE_UNKNOWN: return "PARSE_STATE_UNKNOWN";
-	case PARSE_STATE_EXPL: return "PARSE_STATE_EXPL";
 	case PARSE_STATE_EXPR: return "PARSE_STATE_EXPR";
 	case PARSE_STATE_EXPO: return "PARSE_STATE_EXPO";
-	case PARSE_STATE_POP: return "PARSE_STATE_POP";
 	case PARSE_STATE_COUNT: return "PARSE_STATE_COUNT";
 	default: return "?";
 	}
@@ -99,12 +95,6 @@ typedef struct
 
 
 
-state_t table_expl[]=
-{
-	[TOK_ID] = {PARSE_STATE_EXPO, ACTION_PUSH_ADD_CHILD},
-	[TOK_NUMBER] = {PARSE_STATE_EXPO, ACTION_PUSH_ADD_CHILD},
-	[TOK_COUNT] = {0}
-};
 
 state_t table_expo[]=
 {
@@ -124,7 +114,6 @@ state_t table_expr[]=
 
 state_t * tables[] = 
 {
-	[PARSE_STATE_EXPL] = table_expl,
 	[PARSE_STATE_EXPR] = table_expr,
 	[PARSE_STATE_EXPO] = table_expo,
 	[PARSE_STATE_COUNT] = 0
@@ -134,55 +123,116 @@ state_t * tables[] =
 
 
 
+#define BUFLEN 128
+ecs_entity_t newent(ecs_world_t * world, token_t * token)
+{
+	ecs_entity_t s = ecs_get_scope(world);
+	char buf[BUFLEN];
+	int32_t l = token->length;
+	ecs_entity_t e;
+	switch (token->tok)
+	{
+	case TOK_ID:
+		l = l < BUFLEN ? l : BUFLEN-1;
+		memcpy(buf, token->cursor, l);
+		buf[l] = '\0';
+		e = ecs_new_entity(world, 0);
+		ecs_doc_set_name(world, e, buf);
+		break;
+	
+	default:
+		e = ecs_new_entity(world, 0);
+		ecs_doc_set_name(world, e, tok_t_tostr(token->tok));
+		break;
+	}
+	printf("New %s: %s\n", ecs_doc_get_name(world, s), ecs_doc_get_name(world, e));
+	return e;
+}
+
+
+
+#define STACK_SIZE 128
 
 void ast_parse(ast_context_t * ast)
 {
-	{
-		ecs_entity_t root = ecs_new_entity(ast->world, 0);
-		ecs_doc_set_name(ast->world, root, "ASTROOT");
-		ecs_set_scope(ast->world, root);
-		ast->stack_entity[ast->sp] = root;
-		ast->stack_state[ast->sp] = AST_STATE_ROOT;
-		ast->stack_token[ast->sp] = TOK_UNKNOWN;
-		ast->stack_precedence[ast->sp] = 0;
-	}
+	
+	ecs_entity_t root = ecs_new_entity(ast->world, 0);
+	ecs_doc_set_name(ast->world, root, "ASTROOT");
+	ecs_set_scope(ast->world, root);
+	ast->stack_entity[ast->sp] = root;
+	ast->stack_state[ast->sp] = AST_STATE_ROOT;
+	ast->stack_token[ast->sp] = TOK_UNKNOWN;
+	ast->stack_precedence[ast->sp] = 0;
+	
 
 
 
-	parse_state_t current = PARSE_STATE_EXPL;
+	parse_state_t current = PARSE_STATE_EXPR;
 	action_t action = ACTION_NOP;
-	tok_t stack_tok[100] = {0};
+
+	tok_t stack_tok[STACK_SIZE] = {0};
+	parse_state_t stack_parstate[STACK_SIZE] = {0};
+	ecs_entity_t stack_ents[STACK_SIZE] = {0};
+
 	int32_t sp = 0;
 	stack_tok[sp] = TOK_UNKNOWN;
+	stack_parstate[sp] = current;
+	stack_ents[sp] = root;
+
 	token_t token;
+	ecs_entity_t e;
 	while(1)
 	{
 		printf("State: %s\n", parse_state_t_tostr(current), action_t_tostr(action));
+
 		lexer_next(&ast->lexer, &token);
-		printf("Token: %s\n", tok_t_tostr(token.tok));
+		printf("Token: %s: %s\n", tok_t_tostr(token.tok), token.cursor);
 		if(token.tok == TOK_EOF) {break;}
+
 		action = tables[current][token.tok].action;
-		current = tables[current][token.tok].state;
 		printf("Action: %s\n", action_t_tostr(action));
 
 		switch (action)
 		{
 		case ACTION_PUSH_ADD_CHILD:
+			e = newent(ast->world, &token);
+			ecs_set_scope(ast->world, e);
 			sp++;
 			stack_tok[sp] = token.tok;
+			stack_parstate[sp] = current;
+			stack_ents[sp] = e;
 			break;
 
-		case ACTION_PUSH_ADD_PARENT_PRECEDENCE:
-			{
+		case ACTION_PUSH_ADD_PARENT_PRECEDENCE:{
+				//printf("Remove pair %s EcsChildOf %s\n", ecs_doc_get_name(ast->world, stack_ents[sp]), ecs_doc_get_name(ast->world, stack_ents[sp-1]));
+
 				int32_t p1 = tok_t_precedence[token.tok];
 				int32_t p2 = tok_t_precedence[stack_tok[sp-1]];
-				if(p1 < p2){sp--;}
-			}
-			break;
+				if(p2 && (p1 < p2))
+				{
+					stack_tok[sp] = 0;
+					stack_parstate[sp] = 0;
+					sp--;
+				}
+				
+				ecs_set_scope(ast->world, stack_ents[sp-1]);
+				e = newent(ast->world, &token);
+				printf("Add pair %s EcsChildOf %s\n", ecs_doc_get_name(ast->world, stack_ents[sp]), ecs_doc_get_name(ast->world, e));
+				ecs_add_pair(ast->world, stack_ents[sp], EcsChildOf, e);
+				ecs_set_scope(ast->world, e);
+
+
+				stack_tok[sp] = token.tok;
+				stack_parstate[sp] = current;
+				stack_ents[sp] = e;
+			}break;
 		
 		default:
 			break;
 		}
+
+		
+		current = tables[current][token.tok].state;
 	}
 
 }
