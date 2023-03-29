356cde5d9898709e01b12f6e78cee89f0d777731
Johan Söderlind Åström <johan.soderlind.astrom@gmail.com>
2023-03-29 21:19:00 +0200

    update

--- apps/demo_sokol1/main.c
+++ apps/demo_sokol1/main.c
@@ -1,87 +1,39 @@
-#define SOKOL_GLCORE33
-
-#define SOKOL_IMPL
-#include "sokol_gfx.h"
-#include "sokol_gp.h"
+#include "flecs.h"
 #include "sokol_app.h"
-#include "sokol_glue.h"
+#include "test.h"
 
-#include <stdio.h> // for fprintf()
-#include <stdlib.h> // for exit()
-#include <math.h> // for sinf() and cosf()
 
-// Called on every frame of the application.
-static void frame(void) {
-    // Get current window size.
-    int width = sapp_width(), height = sapp_height();
-    float ratio = width/(float)height;
+//ECS_DECLARE(EgSappDesc);
 
-    // Begin recording draw commands for a frame buffer of size (width, height).
-    sgp_begin(width, height);
-    // Set frame buffer drawing region to (0,0,width,height).
-    sgp_viewport(0, 0, width, height);
-    // Set drawing coordinate space to (left=-ratio, right=ratio, top=1, bottom=-1).
-    sgp_project(-ratio, ratio, 1.0f, -1.0f);
+/*
+int main(int argc, char* argv[])
+{
+    ecs_world_t *world = ecs_init();
+    (void)argc;
+    (void)argv;
 
-    // Clear the frame buffer.
-    sgp_set_color(0.1f, 0.1f, 0.1f, 1.0f);
-    sgp_clear();
+	//ECS_IMPORT(world, EgSapp);
 
-    // Draw an animated rectangle that rotates and changes its colors.
-    float time = sapp_frame_count() * sapp_frame_duration();
-    float r = sinf(time)*0.5+0.5, g = cosf(time)*0.5+0.5;
-    sgp_set_color(r, g, 0.3f, 1.0f);
-    sgp_rotate_at(time, 0.0f, 0.0f);
-    sgp_draw_filled_rect(-0.5f, -0.5f, 1.0f, 1.0f);
 
-    // Begin a render pass.
-    sg_pass_action pass_action = {0};
-    sg_begin_default_pass(&pass_action, width, height);
-    // Dispatch all draw commands to Sokol GFX.
-    sgp_flush();
-    // Finish a draw command queue, clearing it.
-    sgp_end();
-    // End render pass.
-    sg_end_pass();
-    // Commit Sokol render.
-    sg_commit();
+    return ecs_app_run(world, &(ecs_app_desc_t){
+        .target_fps = 60, 
+        .enable_rest = true,
+        .enable_monitor = true
+    });
 }
+*/
 
-// Called when the application is initializing.
-static void init(void) {
-    // Initialize Sokol GFX.
-    sg_desc sgdesc = {.context = sapp_sgcontext()};
-    sg_setup(&sgdesc);
-    if(!sg_isvalid()) {
-        fprintf(stderr, "Failed to create Sokol GFX context!\n");
-        exit(-1);
-    }
 
-    // Initialize Sokol GP, adjust the size of command buffers for your own use.
-    sgp_desc sgpdesc = {0};
-    sgp_setup(&sgpdesc);
-    if(!sgp_is_valid()) {
-        fprintf(stderr, "Failed to create Sokol GP context: %s\n", sgp_get_error_message(sgp_get_last_error()));
-        exit(-1);
-    }
-}
 
-// Called when the application is shutting down.
-static void cleanup(void) {
-    // Cleanup Sokol GP and Sokol GFX resources.
-    sgp_shutdown();
-    sg_shutdown();
-}
-
-// Implement application main through Sokol APP.
-sapp_desc sokol_main(int argc, char* argv[]) {
+sapp_desc sokol_main(int argc, char* argv[])
+{
     (void)argc;
     (void)argv;
     return (sapp_desc){
         .init_cb = init,
         .frame_cb = frame,
         .cleanup_cb = cleanup,
-        .window_title = "Triangle (Sokol GP)",
-        .sample_count = 4, // Enable anti aliasing.
+        .window_title = "Primitives (Sokol GP)",
+        .sample_count = 4,
     };
 }
---
+++ apps/demo_sokol1/test.h
@@ -0,0 +1,8 @@
+#pragma once
+
+void frame(void);
+
+void init(void);
+
+void cleanup(void);
+
