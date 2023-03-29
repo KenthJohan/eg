e1e1c4e116682d33bb64a95cfcdf5d1ff7a5d1ed
Johan Söderlind Åström <johan.soderlind.astrom@gmail.com>
2023-03-29 21:23:05 +0200

    update

---
+++ vendor/sokol_gfx.c
@@ -0,0 +1,7 @@
+#define SOKOL_GLCORE33
+
+#define SOKOL_GFX_IMPL
+#include "sokol_gfx.h"
+
+#define SOKOL_GP_IMPL
+#include "sokol_gp.h"
---
+++ vendor/sokol_glue.c
@@ -0,0 +1,6 @@
+#include "sokol_app.h"
+#include "sokol_gfx.h"
+
+#define SOKOL_GLUE_IMPL
+#define SOKOL_GLCORE33
+#include "sokol_glue.h"
