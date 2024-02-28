274442ea51fd658d30ab61d7d31e9ab74f57b1e5
Johan Söderlind Åström <johan.soderlind.astrom@gmail.com>
2024-02-28 21:43:48 +0100

    update

--- vendor/egwebsockets/src/ws.c
+++ vendor/egwebsockets/src/ws.c
@@ -109,14 +109,17 @@ static void * thread_spam(void *d)
 	struct msg amsg;
 	int len = 128, index = 1, n, whoami = 0;
 
-	for (n = 0; n < (int)LWS_ARRAY_SIZE(vhd->pthread_spam); n++)
-		if (pthread_equal(pthread_self(), vhd->pthread_spam[n]))
+	for (n = 0; n < (int)LWS_ARRAY_SIZE(vhd->pthread_spam); n++) {
+		if (pthread_equal(pthread_self(), vhd->pthread_spam[n])) {
 			whoami = n + 1;
+		}
+	}
 
 	do {
 		/* don't generate output if nobody connected */
-		if (!vhd->pss_list)
+		if (!vhd->pss_list) {
 			goto wait;
+		}
 
 		pthread_mutex_lock(&vhd->lock_ring); /* --------- ring lock { */
 
@@ -132,20 +135,19 @@ static void * thread_spam(void *d)
 			lwsl_user("OOM: dropping\n");
 			goto wait_unlock;
 		}
-		n = lws_snprintf((char *)amsg.payload + LWS_PRE, (unsigned int)len,
-			         "%s: tid: %d, msg: %d", vhd->config,
-			         whoami, index++);
+		n = lws_snprintf((char *)amsg.payload + LWS_PRE, (unsigned int)len, "%s: tid: %d, msg: %d", vhd->config, whoami, index++);
 		amsg.len = (unsigned int)n;
 		n = (int)lws_ring_insert(vhd->ring, &amsg, 1);
 		if (n != 1) {
 			__minimal_destroy_message(&amsg);
 			lwsl_user("dropping!\n");
-		} else
+		} else {
 			/*
 			 * This will cause a LWS_CALLBACK_EVENT_WAIT_CANCELLED
 			 * in the lws service thread context.
 			 */
 			lws_cancel_service(vhd->context);
+		}
 
 wait_unlock:
 		pthread_mutex_unlock(&vhd->lock_ring); /* } ring lock ------- */
@@ -166,32 +168,31 @@ wait:
 
 static int callback_minimal(struct lws *wsi, enum lws_callback_reasons reason,void *user, void *in, size_t len)
 {
-	struct per_session_data__minimal *pss =
-			(struct per_session_data__minimal *)user;
-	struct per_vhost_data__minimal *vhd =
-			(struct per_vhost_data__minimal *)
-			lws_protocol_vh_priv_get(lws_get_vhost(wsi),
-					lws_get_protocol(wsi));
+	struct per_session_data__minimal *pss = (struct per_session_data__minimal *)user;
+	struct per_vhost_data__minimal *vhd = (struct per_vhost_data__minimal *)lws_protocol_vh_priv_get(lws_get_vhost(wsi), lws_get_protocol(wsi));
+
 	const struct lws_protocol_vhost_options *pvo;
 	const struct msg *pmsg;
 	void *retval;
 	int n, m, r = 0;
 
+	char ip[256];
+	if (lws_get_peer_simple(wsi, ip, 16)) {
+		lwsl_notice("Ip: %s\n", ip);
+	};
+
 	switch (reason) {
 	case LWS_CALLBACK_PROTOCOL_INIT:
+		lwsl_notice("LWS_CALLBACK_PROTOCOL_INIT\n");
 		/* create our per-vhost struct */
-		vhd = lws_protocol_vh_priv_zalloc(lws_get_vhost(wsi),
-				lws_get_protocol(wsi),
-				sizeof(struct per_vhost_data__minimal));
+		vhd = lws_protocol_vh_priv_zalloc(lws_get_vhost(wsi), lws_get_protocol(wsi), sizeof(struct per_vhost_data__minimal));
 		if (!vhd)
 			return 1;
 
 		pthread_mutex_init(&vhd->lock_ring, NULL);
 
 		/* recover the pointer to the globals struct */
-		pvo = lws_pvo_search(
-			(const struct lws_protocol_vhost_options *)in,
-			"config");
+		pvo = lws_pvo_search((const struct lws_protocol_vhost_options *)in, "config");
 		if (!pvo || !pvo->value) {
 			lwsl_err("%s: Can't find \"config\" pvo\n", __func__);
 			return 1;
@@ -202,8 +203,7 @@ static int callback_minimal(struct lws *wsi, enum lws_callback_reasons reason,vo
 		vhd->protocol = lws_get_protocol(wsi);
 		vhd->vhost = lws_get_vhost(wsi);
 
-		vhd->ring = lws_ring_create(sizeof(struct msg), 8,
-					    __minimal_destroy_message);
+		vhd->ring = lws_ring_create(sizeof(struct msg), 8, __minimal_destroy_message);
 		if (!vhd->ring) {
 			lwsl_err("%s: failed to create ring\n", __func__);
 			return 1;
@@ -212,8 +212,7 @@ static int callback_minimal(struct lws *wsi, enum lws_callback_reasons reason,vo
 		/* start the content-creating threads */
 
 		for (n = 0; n < (int)LWS_ARRAY_SIZE(vhd->pthread_spam); n++)
-			if (pthread_create(&vhd->pthread_spam[n], NULL,
-					   thread_spam, vhd)) {
+			if (pthread_create(&vhd->pthread_spam[n], NULL, thread_spam, vhd)) {
 				lwsl_err("thread creation failed\n");
 				r = 1;
 				goto init_fail;
@@ -221,19 +220,24 @@ static int callback_minimal(struct lws *wsi, enum lws_callback_reasons reason,vo
 		break;
 
 	case LWS_CALLBACK_PROTOCOL_DESTROY:
+	lwsl_notice("LWS_CALLBACK_PROTOCOL_DESTROY\n");
 init_fail:
 		vhd->finished = 1;
-		for (n = 0; n < (int)LWS_ARRAY_SIZE(vhd->pthread_spam); n++)
-			if (vhd->pthread_spam[n])
+		for (n = 0; n < (int)LWS_ARRAY_SIZE(vhd->pthread_spam); n++) {
+			if (vhd->pthread_spam[n]) {
 				pthread_join(vhd->pthread_spam[n], &retval);
+			}
+		}
 
-		if (vhd->ring)
+		if (vhd->ring){
 			lws_ring_destroy(vhd->ring);
+		}
 
 		pthread_mutex_destroy(&vhd->lock_ring);
 		break;
 
 	case LWS_CALLBACK_ESTABLISHED:
+		lwsl_notice("LWS_CALLBACK_ESTABLISHED\n");
 		/* add ourselves to the list of live pss held in the vhd */
 		lws_ll_fwd_insert(pss, pss_list, vhd->pss_list);
 		pss->tail = lws_ring_get_oldest_tail(vhd->ring);
@@ -241,12 +245,13 @@ init_fail:
 		break;
 
 	case LWS_CALLBACK_CLOSED:
+		lwsl_notice("LWS_CALLBACK_CLOSED\n");
 		/* remove our closing pss from the list of live pss */
-		lws_ll_fwd_remove(struct per_session_data__minimal, pss_list,
-				  pss, vhd->pss_list);
+		lws_ll_fwd_remove(struct per_session_data__minimal, pss_list, pss, vhd->pss_list);
 		break;
 
 	case LWS_CALLBACK_SERVER_WRITEABLE:
+		lwsl_notice("LWS_CALLBACK_SERVER_WRITEABLE\n");
 		pthread_mutex_lock(&vhd->lock_ring); /* --------- ring lock { */
 
 		pmsg = lws_ring_get_element(vhd->ring, &pss->tail);
@@ -256,8 +261,7 @@ init_fail:
 		}
 
 		/* notice we allowed for LWS_PRE in the payload already */
-		m = lws_write(wsi, ((unsigned char *)pmsg->payload) + LWS_PRE,
-			      pmsg->len, LWS_WRITE_TEXT);
+		m = lws_write(wsi, ((unsigned char *)pmsg->payload) + LWS_PRE, pmsg->len, LWS_WRITE_TEXT);
 		if (m < (int)pmsg->len) {
 			pthread_mutex_unlock(&vhd->lock_ring); /* } ring lock ------- */
 			lwsl_err("ERROR %d writing to ws socket\n", m);
@@ -275,17 +279,20 @@ init_fail:
 		);
 
 		/* more to do? */
-		if (lws_ring_get_element(vhd->ring, &pss->tail))
+		if (lws_ring_get_element(vhd->ring, &pss->tail)) {
 			/* come back as soon as we can write more */
 			lws_callback_on_writable(pss->wsi);
+		}
 
 		pthread_mutex_unlock(&vhd->lock_ring); /* } ring lock ------- */
 		break;
 
 	case LWS_CALLBACK_RECEIVE:
+		lwsl_notice("LWS_CALLBACK_RECEIVE\n");
 		break;
 
 	case LWS_CALLBACK_EVENT_WAIT_CANCELLED:
+		lwsl_notice("LWS_CALLBACK_EVENT_WAIT_CANCELLED\n");
 		if (!vhd)
 			break;
 		/*
@@ -296,8 +303,7 @@ init_fail:
 		 * We respond by scheduling a writable callback for all
 		 * connected clients.
 		 */
-		lws_start_foreach_llp(struct per_session_data__minimal **,
-				      ppss, vhd->pss_list) {
+		lws_start_foreach_llp(struct per_session_data__minimal **, ppss, vhd->pss_list) {
 			lws_callback_on_writable((*ppss)->wsi);
 		} lws_end_foreach_llp(ppss, pss_list);
 		break;
@@ -309,18 +315,27 @@ init_fail:
 	return r;
 }
 
-#define LWS_PLUGIN_PROTOCOL_MINIMAL \
-	{ \
-		"lws-minimal", \
-		callback_minimal, \
-		sizeof(struct per_session_data__minimal), \
-		128, \
-		0, NULL, 0 \
-	}
+
 
 static struct lws_protocols protocols[] = {
-	{ "http", lws_callback_http_dummy, 0, 0, 0, NULL, 0 },
-	LWS_PLUGIN_PROTOCOL_MINIMAL,
+	{ 
+		.name = "http", 
+		.callback = lws_callback_http_dummy, 
+		.per_session_data_size = 0, 
+		.rx_buffer_size = 0, 
+		.id = 0, 
+		.user = NULL, 
+		.tx_packet_size = 0 
+	},
+	{
+		.name = "lws-minimal",
+		.callback = callback_minimal,
+		.per_session_data_size = sizeof(struct per_session_data__minimal),
+		.rx_buffer_size = 128,
+		.id = 0,
+		.user = NULL,
+		.tx_packet_size = 0
+	},
 	LWS_PROTOCOL_LIST_TERM
 };
 
@@ -346,17 +361,17 @@ static const struct lws_http_mount mount = {
  */
 
 static const struct lws_protocol_vhost_options pvo_ops = {
-	NULL,
-	NULL,
-	"config",		/* pvo name */
-	(void *)"myconfig"	/* pvo value */
+	.next = NULL,
+	.options = NULL,
+	.name = "config",		/* pvo name */
+	.value = (void *)"myconfig"	/* pvo value */
 };
 
 static const struct lws_protocol_vhost_options pvo = {
-	NULL,		/* "next" pvo linked-list */
-	&pvo_ops,	/* "child" pvo linked-list */
-	"lws-minimal",	/* protocol name we belong to on this vhost */
-	""		/* ignored */
+	.next = NULL,		/* "next" pvo linked-list */
+	.options = &pvo_ops,	/* "child" pvo linked-list */
+	.name = "lws-minimal",	/* protocol name we belong to on this vhost */
+	.value = ""		/* ignored */
 };
 
 void sigint_handler(int sig)
@@ -400,8 +415,7 @@ void * server_thread(void* arg)
 	info.mounts = &mount;
 	info.protocols = protocols;
 	info.pvo = &pvo; /* per-vhost options */
-	info.options =
-		LWS_SERVER_OPTION_HTTP_HEADERS_SECURITY_BEST_PRACTICES_ENFORCE;
+	info.options = LWS_SERVER_OPTION_HTTP_HEADERS_SECURITY_BEST_PRACTICES_ENFORCE;
 
 	context = lws_create_context(&info);
 	if (!context) {
@@ -411,9 +425,11 @@ void * server_thread(void* arg)
 
 	/* start the threads that create content */
 
-	while (!interrupted)
-		if (lws_service(context, 0))
+	while (!interrupted) {
+		if (lws_service(context, 0)) {
 			interrupted = 1;
+		}
+	}
 
 	lws_context_destroy(context);
 
