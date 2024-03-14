2b0adeb35e16d3a2a86498a531005eebee808974
Johan Söderlind Åström <johan.soderlind.astrom@gmail.com>
2024-03-14 21:19:49 +0100

    update

--- vendor/eglws/src/spam.c
+++ vendor/eglws/src/spam.c
@@ -5,67 +5,23 @@
 static void * thread_spam(void *d)
 {
 	eglws_vhd_t *vhd = (eglws_vhd_t *)d;
-	eglws_msg_t amsg;
-	int len = 128;
 	int index = 1;
 	int whoami = 0;
-
 	for (int n = 0; n < (int)LWS_ARRAY_SIZE(vhd->spam_pthread); n++) {
 		if (pthread_equal(pthread_self(), vhd->spam_pthread[n])) {
 			whoami = n + 1;
 		}
 	}
-
 	do {
-		// don't generate output if nobody connected
-		if (!vhd->pss_list) {
-			goto wait;
+		char buf[256];
+		snprintf(buf, sizeof(buf), "%s: tid: %d, msg: %d", vhd->config, whoami, index++);
+		int rc = eglws_vhd_send_text(vhd, buf);
+		if(rc) {
+			usleep(1000*1000*2);
 		}
-
-		pthread_mutex_lock(&vhd->lock_ring);
-
-		{
-			// only create if space in ringbuffer
-			int n = (int)lws_ring_get_count_free_elements(vhd->ring);
-			if (!n) {
-				lwsl_user("dropping!\n");
-				goto wait_unlock;
-			}
-		}
-
-		amsg.payload = malloc((unsigned int)(LWS_PRE + len));
-		if (!amsg.payload) {
-			lwsl_user("OOM: dropping\n");
-			goto wait_unlock;
-		}
-
-		{
-			int n = lws_snprintf((char *)amsg.payload + LWS_PRE, (unsigned int)len, "%s: tid: %d, msg: %d", vhd->config, whoami, index++);
-			amsg.len = (unsigned int)n;
-			n = (int)lws_ring_insert(vhd->ring, &amsg, 1);
-			if (n != 1) {
-				eglws_msg_fini(&amsg);
-				lwsl_user("dropping!\n");
-			} else {
-				// This will cause a LWS_CALLBACK_EVENT_WAIT_CANCELLED in the lws service thread context.
-				lws_cancel_service(vhd->context);
-			}
-		}
-
-		printf("lws_ring_insert %li\n", lws_ring_get_count_waiting_elements(vhd->ring, NULL));
-
-wait_unlock:
-		pthread_mutex_unlock(&vhd->lock_ring);
-
-wait:
-		usleep(1000*1000*2);
-
 	} while (!vhd->spam_finished);
-
 	lwsl_notice("thread_spam %d exiting\n", whoami);
-
 	pthread_exit(NULL);
-
 	return NULL;
 }
 
