fd6303079dc15e487ddce9723db880629c472c74
Johan Söderlind Åström <johan.soderlind.astrom@gmail.com>
2024-05-14 17:15:31 +0200

    added odrive can example

---
+++ apps/demo_can2/src/eg_can.c
@@ -0,0 +1,123 @@
+// #define _GNU_SOURCE /* To get defns of NI_MAXSERV and NI_MAXHOST */
+
+// Fixes: error: variable ‘ifr’ has initializer but incomplete type
+#ifndef _DEFAULT_SOURCE
+#define _DEFAULT_SOURCE
+#endif
+
+#include "eg_can.h"
+
+#include <net/if.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+
+#include <linux/can.h>
+#include <linux/can/raw.h>
+#include <sys/epoll.h>
+#include <errno.h>
+#include <assert.h>
+
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <ifaddrs.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <linux/if_link.h>
+#include <string.h>
+
+
+
+
+int interface_index_from_name(int s, char const *interface)
+{
+	int rc = 0;
+	struct ifreq ifr = {0};
+	strcpy(ifr.ifr_name, interface);
+	rc = ioctl(s, SIOCGIFINDEX, &ifr);
+	if (rc < 0) {
+		return rc;
+	}
+	return ifr.ifr_ifindex;
+}
+
+int socket_from_interace(char const *interface)
+{
+	printf("CAN Sockets Demo\r\n");
+	int s = socket(PF_CAN, SOCK_RAW, CAN_RAW);
+	if (s < 0) {
+		perror("socket()");
+		return 1;
+	}
+
+	int index = interface_index_from_name(s, interface);
+	if (index < 0) {
+		return index;
+	}
+
+	struct sockaddr_can addr = {0};
+	addr.can_family = AF_CAN;
+	addr.can_ifindex = index;
+
+	if (bind(s, (struct sockaddr *)&addr, sizeof(struct sockaddr_can)) < 0) {
+		perror("bind()");
+		return 1;
+	}
+	return s;
+}
+
+
+
+#define EG_CAN_CTRLMSG_LEN CMSG_SPACE(sizeof(struct timeval)) + CMSG_SPACE(3 * sizeof(struct timespec)) + CMSG_SPACE(sizeof(__u32))
+/* CAN CC/FD/XL frame union */
+typedef union {
+	struct can_frame cc;
+	struct canfd_frame fd;
+	// struct canxl_frame xl;
+} cu_t;
+
+int eg_can_recv(int s, eg_can_frame_t *frame)
+{
+	cu_t cu = {0};
+	struct iovec iov[1] = {0};
+	struct msghdr msg = {0};
+	char ctrlmsg[EG_CAN_CTRLMSG_LEN] = {0};
+	struct sockaddr_can addr = {0};
+
+	iov[0].iov_base = &cu;
+
+	msg.msg_name = &addr;
+	msg.msg_iov = iov;
+	msg.msg_iovlen = 1;
+	msg.msg_control = &ctrlmsg;
+	/* these settings may be modified by recvmsg() */
+	msg.msg_iov[0].iov_len = sizeof(cu_t);
+	msg.msg_namelen = sizeof(struct sockaddr_can);
+	msg.msg_controllen = EG_CAN_CTRLMSG_LEN;
+	msg.msg_flags = 0;
+	int nbytes = recvmsg(s, &msg, 0);
+	if (nbytes < 0) {
+		perror("recvmsg()");
+		return nbytes;
+	}
+	// eg_can_print((cu_t *)msg.msg_iov->iov_base, nbytes);
+
+	frame->can_id = cu.cc.can_id;
+	frame->len = cu.cc.len;
+	memcpy(frame->data, cu.cc.data, CAN_MAX_DLEN);
+
+	return nbytes;
+}
+
+
+int eg_can_send(int s, eg_can_frame_t *frame)
+{
+	assert(sizeof(eg_can_frame_t) == sizeof(struct can_frame));
+	int n = write(s, frame, sizeof(eg_can_frame_t));
+	if (n != sizeof(eg_can_frame_t)) {
+		perror("write()");
+		return n;
+	}
+	return n;
+}
---
+++ apps/demo_can2/src/main.c
@@ -0,0 +1,123 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/epoll.h>
+#include <errno.h>
+#include <string.h>
+#include "eg_can.h"
+
+typedef struct {
+	int s;
+} bundled_data_per_socket_t;
+
+
+#define MAXSOCK 16
+
+typedef struct {
+	int fd_epoll;
+	bundled_data_per_socket_t socks[MAXSOCK];
+} thread_data_t;
+
+
+#define MAXSOCK 16
+static void *thread_loop(thread_data_t * arg)
+{
+	struct epoll_event events[MAXSOCK] = {0};
+	int timeout_ms = -1;
+	int running = 1;
+	while (running) {
+
+		// Wait for events:
+		int num_events = epoll_wait(arg->fd_epoll, events, MAXSOCK, timeout_ms);
+		if (num_events < 0) {
+			fprintf(stderr, "close(): %i: %s", num_events, strerror(errno));
+			if (errno != EINTR) {
+				running = 0;
+			}
+			continue;
+		}
+
+		// Check every event:
+		for (int i = 0; i < num_events; i++) {
+			bundled_data_per_socket_t *obj = events[i].data.ptr;
+			if (events[i].events & EPOLLERR) {
+				fprintf(stderr, "EPOLLERR\n");
+			}
+			eg_can_frame_t frame = {0};
+			int rc = eg_can_recv(obj->s, &frame);
+			if (rc < 0) {
+				fprintf(stderr, "eg_can_recv(): %i: %s", rc, strerror(errno));
+				continue;
+			}
+		}
+	}
+
+	return NULL;
+}
+
+
+#define ODRIVE_CANID(node_id, cmd_id) (((node_id) << 5) | (cmd_id))
+#define NODEID 2
+
+static void send_command(int s)
+{
+	eg_can_frame_t frame = {0};
+	frame.can_id = ODRIVE_CANID(NODEID, 0);
+	eg_can_send(s, &frame);
+}
+
+
+static void send_vel(int s, float velocity, float torque)
+{
+	eg_can_frame_t frame = {0};
+	frame.can_id = ODRIVE_CANID(NODEID, 0x0D);
+	frame.len = 8;
+	memcpy(frame.data + 0, &velocity, 4);
+	memcpy(frame.data + 4, &torque, 4);
+	eg_can_send(s, &frame);
+}
+
+
+
+int main(int argc, char const * argv[])
+{
+	thread_data_t tdata = {0};
+	tdata.fd_epoll = epoll_create(1);
+	if (tdata.fd_epoll < 0) {
+		fprintf(stderr, "epoll_create(): %i: %s", tdata.fd_epoll, strerror(errno));
+		return -1;
+	}
+
+
+	int index = 0;
+
+	{
+		printf("Create socket:\n");
+		int s = socket_from_interace("can0");
+		printf("socket %i\n", s);
+		tdata.socks[index].s = s;
+		//send_command(s);
+		send_vel(s, 1.0f, 1.0f);
+		return 0;
+		
+	}
+
+
+	{
+		printf("Adding socket to epoll:\n");
+		struct epoll_event setup = {0};
+		setup.events = EPOLLIN;
+		setup.data.ptr = tdata.socks + index;
+		int rc = epoll_ctl(tdata.fd_epoll, EPOLL_CTL_ADD, tdata.socks[index].s, &setup);
+		if (rc) {
+			fprintf(stderr, "epoll_ctl(): %i: %s", rc, strerror(errno));
+		}
+	}
+
+	{
+		printf("Starting epoll thread!\n");
+		thread_loop(&tdata);
+	}
+	
+}
+
