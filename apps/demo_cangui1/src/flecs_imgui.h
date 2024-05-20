e82cec322a4f9b88e7d2348eb6fc94b107e0ed8b
Johan Söderlind Åström <johan.soderlind.astrom@gmail.com>
2024-05-20 19:18:29 +0200

    update

---
+++ apps/demo_cangui1/src/flecs_imgui.h
@@ -0,0 +1,21 @@
+#pragma once
+#define CIMGUI_DEFINE_ENUMS_AND_STRUCTS
+#include <cimgui.h>
+#include <flecs.h>
+#include <egquantities.h>
+
+
+bool igCombo_flecs(ecs_world_t *world, ecs_entity_t parent, int *parent_val);
+
+
+char const *flecs_get_type(ecs_primitive_kind_t kind);
+
+size_t djb_hash(const char *cp);
+
+void igPushStyleColor_U32_HSV_strhash(const char *cp);
+
+void igPushStyleColor_U32_HSV_hash32(uint32_t value);
+
+bool igSlider_flecs(const char *label, EgQuantitiesRangedGeneric *value);
+
+void igInput_flecs(const char *label, eg_generic_number_t *val, ecs_primitive_kind_t kind);
--- apps/demo_cangui1/src/gui_signals.c
+++ apps/demo_cangui1/src/gui_signals.c
@@ -5,7 +5,6 @@
 
 #include <egcolors/eg_color.h>
 
-#define COLOR_RGBA(r, g, b, a) ((r) << 0 | (g) << 8 | (b) << 16 | (a) << 24)
 
 #define CIMGUI_DEFINE_ENUMS_AND_STRUCTS
 #include <cimgui.h>
@@ -14,6 +13,7 @@
 #include <float.h>
 #include <math.h>
 #include "GuiCan.h"
+#include "flecs_imgui.h"
 
 typedef struct {
 	char const *name;
@@ -25,178 +25,13 @@ typedef struct {
 	ecs_entity_t e;
 } gui_can_table_t;
 
-static void igSlider_flecs(const char *label, eg_can_book_t *book, EgCanSignal *signal, EgQuantitiesRangedGeneric *value)
-{
-	bool modified = false;
-	switch (value->kind) {
-	case EcsF32:
-		if (isfinite(value->tx.val_f32) && isfinite(value->min.val_f32) && isfinite(value->max.val_f32)) {
-			igPushItemWidth(-1);
-			modified = igSliderScalar(label, ImGuiDataType_Float, &value->tx.val_f32, &value->min.val_f32, &value->max.val_f32, "%f", 0);
-			igPopItemWidth();
-		}
-		break;
-	case EcsU8:
-		igPushItemWidth(-1);
-		modified = igSliderScalar(label, ImGuiDataType_U8, &value->tx.val_u8, &value->min.val_u8, &value->max.val_u8, "%u", 0);
-		igPopItemWidth();
-		break;
-	case EcsU16:
-		igPushItemWidth(-1);
-		modified = igSliderScalar(label, ImGuiDataType_U16, &value->tx.val_u16, &value->min.val_u16, &value->max.val_u16, "%u", 0);
-		igPopItemWidth();
-		break;
-	case EcsU32:
-		igPushItemWidth(-1);
-		modified = igSliderScalar(label, ImGuiDataType_U32, &value->tx.val_u32, &value->min.val_u32, &value->max.val_u32, "%u", 0);
-		igPopItemWidth();
-		break;
 
-	default:
-		break;
-	}
-	if (modified) {
-		EgCan_book_prepare_send(book, signal, value);
-	}
-}
 
-static void igInput_flecs(const char *label, eg_generic_number_t *val, ecs_primitive_kind_t kind)
-{
-	switch (kind) {
-	case EcsF64:
-		igPushItemWidth(-1);
-		igInputDouble("#1", &val->val_f64, 0, 0, "%f", 0);
-		igPopItemWidth();
-		break;
-	case EcsF32:
-		igPushItemWidth(-1);
-		igInputFloat("#1", &val->val_f32, 0, 0, "%f", 0);
-		igPopItemWidth();
-		break;
-	case EcsU8:
-		igPushItemWidth(-1);
-		igInputScalar("#1", ImGuiDataType_U8, &val->val_u8, 0, 0, "%u", 0);
-		igPopItemWidth();
-		break;
-	case EcsU16:
-		igPushItemWidth(-1);
-		igInputScalar("#1", ImGuiDataType_U16, &val->val_u16, 0, 0, "%u", 0);
-		igPopItemWidth();
-		break;
-	case EcsU32:
-		igPushItemWidth(-1);
-		igInputScalar("#1", ImGuiDataType_U32, &val->val_u32, 0, 0, "%u", 0);
-		igPopItemWidth();
-		break;
-	default:
-		break;
-	}
-}
 
-static char const *get_type(ecs_primitive_kind_t kind)
-{
-	switch (kind) {
-	case EcsBool:
-		return "Bool";
-	case EcsChar:
-		return "Char";
-	case EcsByte:
-		return "Byte";
-	case EcsU8:
-		return "U8";
-	case EcsU16:
-		return "U16";
-	case EcsU32:
-		return "U32";
-	case EcsU64:
-		return "U64";
-	case EcsI8:
-		return "I8";
-	case EcsI16:
-		return "I16";
-	case EcsI32:
-		return "I32";
-	case EcsI64:
-		return "I64";
-	case EcsF32:
-		return "F32";
-	case EcsF64:
-		return "F64";
-	case EcsUPtr:
-		return "UPtr";
-	case EcsIPtr:
-		return "IPtr";
-	case EcsString:
-		return "String";
-	case EcsEntity:
-		return "Entity";
-	case EcsId:
-		return "Id";
-	}
-	return "";
-}
 
-static size_t djb_hash(const char *cp)
-{
-	size_t hash = 5381;
-	while (*cp)
-		hash = 33 * hash ^ (unsigned char)*cp++;
-	return hash;
-}
 
-static void igPushStyleColor_U32_HSV_strhash(const char *cp)
-{
-	size_t h = djb_hash(cp);
-	uint8_t r;
-	uint8_t g;
-	uint8_t b;
-	eg_color_hsv_to_rgb(h, h >> 16, 255, &r, &g, &b);
-	igPushStyleColor_U32(ImGuiCol_Text, COLOR_RGBA(r, g, b, 255));
-}
 
-static void igPushStyleColor_U32_HSV_hash32(uint32_t value)
-{
-	size_t hash;
-	hash = 5381;
-	hash = 14 * hash ^ value;
-	hash = 546567 * hash ^ value;
-	uint8_t h = hash;
-	hash = 5381;
-	hash = 33 * hash ^ value;
-	hash = 33333 * hash ^ value;
-	uint8_t s = hash;
-	uint8_t r;
-	uint8_t g;
-	uint8_t b;
-	eg_color_hsv_to_rgb(h, s, 255, &r, &g, &b);
-	igPushStyleColor_U32(ImGuiCol_Text, COLOR_RGBA(r, g, b, 255));
-}
 
-static bool igCombo_flecs(ecs_world_t *world, ecs_entity_t parent, int *parent_val)
-{
-	bool selected = false;
-	const EcsEnum *enum_type = ecs_get(world, parent, EcsEnum);
-	ecs_enum_constant_t *c0 = ecs_map_get_deref(&enum_type->constants, ecs_enum_constant_t, (ecs_map_key_t)*parent_val);
-	igPushItemWidth(-1);
-	igPushID_Ptr((void *)(intptr_t)parent);
-	if (igBeginCombo("", c0 ? c0->name : NULL, 0)) {
-		ecs_map_iter_t it = ecs_map_iter(&enum_type->constants);
-		while (ecs_map_next(&it)) {
-			ecs_enum_constant_t *c = ecs_map_ptr(&it);
-			selected = c->value == c0->value;
-			selected = igSelectable_Bool(c->name, selected, 0, (ImVec2){0, 0});
-			if(selected) {
-				*parent_val = c->value;
-				break;
-			}
-		}
-		igEndCombo();
-	}
-	igPopID();
-	igPopItemWidth();
-
-	return selected;
-}
 
 void gui_signals_progress(ecs_world_t *world, ecs_query_t *q)
 {
@@ -323,8 +158,8 @@ void gui_signals_progress(ecs_world_t *world, ecs_query_t *q)
 			igTableNextColumn();
 			igText("%i", signal->byte_offset);
 			igTableNextColumn();
-			igPushStyleColor_U32_HSV_strhash(get_type(value->kind));
-			igText("%s", get_type(value->kind));
+			igPushStyleColor_U32_HSV_strhash(flecs_get_type(value->kind));
+			igText("%s", flecs_get_type(value->kind));
 			igPopStyleColor(1);
 
 			igTableNextColumn();
@@ -345,7 +180,10 @@ void gui_signals_progress(ecs_world_t *world, ecs_query_t *q)
 					value->tx.val_u64 = (int32_t)selected;
 				}
 			} else {
-				igSlider_flecs("##s1", book, signal, value);
+				bool modifed = igSlider_flecs("##s1", value);
+				if (modifed) {
+					EgCan_book_prepare_send(book, signal, value);
+				}
 			}
 
 			/*
