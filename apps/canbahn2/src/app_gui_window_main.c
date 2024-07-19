55b58889825c0fc4c590850773963b77dc22a82a
Johan Söderlind Åström <johan.soderlind.astrom@gmail.com>
2024-07-19 14:00:37 +0200

    update

--- apps/canbahn2/src/app_gui_window_main.c
+++ apps/canbahn2/src/app_gui_window_main.c
@@ -33,43 +33,116 @@ static void ig_debug_draw()
 	}
 }
 
-static void show_table()
+
+/*
+https://www.csselectronics.com/pages/dbc-editor-can-bus-database
+*/
+static void show_table1(uint8_t value[], int length)
 {
 	ImGuiTableFlags flags = ImGuiTableFlags_Borders | ImGuiTableFlags_SizingFixedFit | ImGuiTableFlags_BordersOuter | ImGuiTableFlags_BordersV | ImGuiTableFlags_ContextMenuInBody | ImGuiTableFlags_NoHostExtendX;
-	if (igBeginTable("table1", 9, flags, (ImVec2){(0.0F), (0.0F)}, (0.0F))) {
+	if (igBeginTable("Message", 9, flags, (ImVec2){(0.0F), (0.0F)}, (0.0F)) == false) {
+		return;
+	}
 
+	igTableNextRow(0, 0);
+	for (int column = 0; column < 8; column++) {
+		igTableSetColumnIndex(column + 1);
+		igText("b%i", column);
+		igTableSetBgColor(ImGuiTableBgTarget_CellBg, IM_COL32(50, 50, 50, 255), -1);
+	}
+
+	for (int row = 0; row < length; row++) {
 		igTableNextRow(0, 0);
+
+		// Fill cells
+		igTableSetColumnIndex(0);
+		igText("%02X", row);
+		igTableSetBgColor(ImGuiTableBgTarget_CellBg, IM_COL32(50, 50, 50, 255), -1);
+
 		for (int column = 0; column < 8; column++) {
-			igTableSetColumnIndex(column+1);
-			igText("b%i", column);
-		}
+			igTableSetColumnIndex(column + 1);
+			// igText("%c%c", 'A' + row, '0' + column);
+			char buf[128];
+			snprintf(buf, 128, "%c%c", 'A' + row, '0' + column);
+			igSelectable_Bool(buf, false, 0, (ImVec2){0, 0});
 
-		for (int row = 0; row < 9; row++) {
-			igTableNextRow(0, 0);
-
-			// Fill cells
-			igTableSetColumnIndex(0);
-			igText("B%i", row);
-			for (int column = 0; column < 8; column++) {
-				igTableSetColumnIndex(column+1);
-				//igText("%c%c", 'A' + row, '0' + column);
-				char buf[128];
-				snprintf(buf, 128, "%c%c", 'A' + row, '0' + column);
-				igSelectable_Bool(buf, false, 0, (ImVec2){0,0});
-
-				// Change background of Cells B1->C2
-				// Demonstrate setting a cell background color with 'ImGui::TableSetBgColor(ImGuiTableBgTarget_CellBg, ...)'
-				// (the CellBg color will be blended over the RowBg and ColumnBg colors)
-				// We can also pass a column number as a third parameter to TableSetBgColor() and do this outside the column loop.
-				if (row >= 1 && row <= 2 && column >= 1 && column <= 2) {
-					ImU32 cell_bg_color = igGetColorU32_Vec4((ImVec4){0.3f, 0.3f, 0.7f, 0.65f});
-					igTableSetBgColor(ImGuiTableBgTarget_CellBg, cell_bg_color, -1);
-				}
+			// Change background of Cells B1->C2
+			// Demonstrate setting a cell background color with 'ImGui::TableSetBgColor(ImGuiTableBgTarget_CellBg, ...)'
+			// (the CellBg color will be blended over the RowBg and ColumnBg colors)
+			// We can also pass a column number as a third parameter to TableSetBgColor() and do this outside the column loop.
+			if (row >= 1 && row <= 2 && column >= 1 && column <= 2) {
+				ImU32 cell_bg_color = igGetColorU32_Vec4((ImVec4){0.3f, 0.3f, 0.7f, 0.65f});
+				igTableSetBgColor(ImGuiTableBgTarget_CellBg, cell_bg_color, -1);
 			}
 		}
-		igEndTable();
 	}
-	ig_debug_draw();
+	igEndTable();
+}
+
+static void show_table2(uint8_t value[], int length)
+{
+	ImGuiTableFlags flags = ImGuiTableFlags_Borders | ImGuiTableFlags_Resizable | ImGuiTableFlags_Reorderable | ImGuiTableFlags_Hideable;
+	if (igBeginTable("Signals", 11, flags, (ImVec2){0, 0}, 0) == false) {
+		return;
+	}
+	// igTableSetupColumn("bus", ImGuiTableColumnFlags_WidthFixed | ImGuiTableColumnFlags_DefaultHide, 50, 0);
+	// igTableSetupColumn("sock", ImGuiTableColumnFlags_WidthFixed | ImGuiTableColumnFlags_DefaultHide, 50, 0);
+	igTableSetupColumn("Name", ImGuiTableColumnFlags_WidthFixed, 100, 0);
+	igTableSetupColumn("Type", ImGuiTableColumnFlags_WidthFixed, 100, 0);
+	igTableSetupColumn("Order", ImGuiTableColumnFlags_WidthFixed, 100, 0);
+	igTableSetupColumn("Mode", ImGuiTableColumnFlags_WidthFixed, 100, 0);
+	igTableSetupColumn("Start", ImGuiTableColumnFlags_WidthFixed, 100, 0);
+	igTableSetupColumn("Length", ImGuiTableColumnFlags_WidthFixed, 100, 0);
+	igTableSetupColumn("Factor", ImGuiTableColumnFlags_WidthFixed, 100, 0);
+	igTableSetupColumn("Offset", ImGuiTableColumnFlags_WidthFixed, 100, 0);
+	igTableSetupColumn("Min", ImGuiTableColumnFlags_WidthFixed, 100, 0);
+	igTableSetupColumn("Max", ImGuiTableColumnFlags_WidthFixed, 100, 0);
+	igTableSetupColumn("Unit", ImGuiTableColumnFlags_WidthFixed, 100, 0);
+	igTableHeadersRow();
+	static char buf[128];
+	static int start;
+	for (int i = 0; i < length; ++i) {
+
+		igTableNextColumn();
+        igPushID_Int(i);
+		igPushItemWidth(-1);
+		igInputText("##Name", buf, 128, 0, 0, 0);
+		igPopItemWidth();
+        igPopID();
+
+		igTableNextColumn();
+		igText("hej2");
+
+		igTableNextColumn();
+		igText("hej3");
+
+		igTableNextColumn();
+		igText("hej4");
+
+		igTableNextColumn();
+        igPushID_Int(i);
+		igPushItemWidth(-1);
+		igInputInt("##Start", &start, 0, 0, 0);
+		igPopItemWidth();
+        igPopID();
+
+		igTableNextColumn();
+		igText("hej6");
+		igTableNextColumn();
+		igText("hej7");
+		igTableNextColumn();
+		igText("hej8");
+		igTableNextColumn();
+		igText("hej9");
+		igTableNextColumn();
+		igText("hej10");
+		igTableNextColumn();
+		igText("hej11");
+	}
+
+	igEndTable();
+
+	// ig_debug_draw();
 }
 
 void app_gui_window_main_init(app_t *app)
@@ -108,7 +181,9 @@ void app_gui_window_main(app_t *app)
 	if (igBeginTabBar("tabs", 0)) {
 		// igText("Hello");
 		if (igBeginTabItem("Message", NULL, 0)) {
-			show_table();
+			show_table1(NULL, 64);
+			igSameLine(0,10);
+			show_table2(NULL, 64);
 			igEndTabItem();
 		}
 		if (igBeginTabItem("Tab2", NULL, 0)) {
