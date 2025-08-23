4816bc0ebf078db29a9f475d22ad10d9c16f678f
Johan Söderlind Åström <johan.soderlind.astrom@gmail.com>
2025-08-23 18:11:30 +0200

    update

--- ecsx/include/ecsx/ecsx_ser.h
+++ ecsx/include/ecsx/ecsx_ser.h
@@ -2,5 +2,3 @@
 #include <flecs.h>
 
 int ecsx_flecs_expr_ser_primitive(const ecs_world_t *world, ecs_primitive_kind_t kind, const void *base, ecs_strbuf_t *str, bool is_expr);
-
-int ecsx_expr_ser_bitmask(const ecs_world_t *world, ecs_entity_t type, const void *ptr, ecs_strbuf_t *str);
